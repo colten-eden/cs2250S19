1e7b61cc3d43fe2605794824d1f4b6797dce7fbb
Colten Eden <colteneden@mail.weber.edu>
2019-01-24 09:07:34 -0700

    Update

--- mod3/strings.c
+++ mod3/strings.c
@@ -23,15 +23,19 @@ int main()
     //A string is a list/collection of characters (array)
     //Strings use %s as the placeholder
     char fName[SIZE];
+    unsigned int age; // For unsigned use the %du, %lu, %llu
     char lName[SIZE];
     printf("Please enter your first name:(< %d characters, No space) ", SIZE);
     //When using arrays, DO NOT include the '&' operator
     //in your scanf
     scanf("%s", fName);
+
+    printf("Enger your age: ");
+    scanf("%u", &age);
     printf("Please enter your last name:(< %d characters, No space) ", SIZE);
     printf("Hi [%s]", fName);
     scanf("%s", lName);
-    printf("Hi [%s][%s]\n", fName, lName);
+    printf("Hi [%s][%s] [%u] years old\n", fName, lName, age);
 
     return 0;
 }
