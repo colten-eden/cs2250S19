dfe976809931c8ab37d8c36de94eff2ee6e6ff4d
Colten Eden <colteneden@mail.weber.edu>
2019-01-24 08:58:36 -0700

    Calculated the cart coordinates

--- mod3/cartesian.c
+++ mod3/cartesian.c
@@ -17,21 +17,26 @@
  * =====================================================================================
  */
 #include <stdio.h>
-
+#include <math.h>
 // Constants
-
+const double  PI = 3.14159;
 
 // Main Function
 int main()
-{
+{   double radius, theta, x_pos, y_pos;
     //1)Capture user input for radius, angle, and theta (degrees)
-    printf("Enter a radius and angle (in degrees) \n");
+    printf("Enter a radius: ");
+    scanf("%lf", &radius);
+    printf("Enter your theta angle in degrees: ");
+    scanf("%lf", &theta);
     //2) Convert the angle to radians
-    
+    theta = (theta * PI/180.0);
     //3) Calculate your cartesian coordinates
-    
-    //4) Print the result
-
+    x_pos = radius*cos(theta);
+    y_pos = radius*sin(theta);
+    //4) print the results
+    printf("Your cartesian coordinates, x = [%lf], y = [%lf]\n",
+            x_pos, y_pos);
     return 0;
 }
 // Function Definitions
