4133cff7d87849e17df679e3d0cedd09519c2e15
Colten Eden <colteneden@mail.weber.edu>
2019-02-19 09:50:25 -0700

    Added finding the sum

--- mod7/hello_func.c
+++ mod7/hello_func.c
@@ -23,10 +23,13 @@
 void Hello_World();
 void Hello_Number(int num);
 int Get_Square(int num);
+int Get_Number();
+int Get_Sum(int num1,int num2);
 // Main Function
 int main(int argc, char* argv[])
 {
     int i1 = 23;
+    int num1, num2, sum;
     Hello_World();
 
     Hello_Number(i1);
@@ -34,6 +37,15 @@ int main(int argc, char* argv[])
     i1 = Get_Square(i1);
     Hello_Number(i1);
     
+    i1 = Get_Number();
+    Hello_Number(i1);
+
+    num1 = Get_Number();
+    Hello_Number(num1);
+    num2 = Get_Number();
+    Hello_Number(num2);
+    sum = Get_Sum(num1, num2);
+    Hello_Number(sum);
     return 0;
 }
 
@@ -83,3 +95,39 @@ int Get_Square(int num)
 square = num * num;
     return square;
 }
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Get_Number
+ *  Description:  Ask user to enter an int and return it
+ *        Param: none
+ *       Return: User int input
+ * =====================================================================================
+ */
+int Get_Number()
+{ 
+    int input;
+    // Algorithm
+    printf("Please enter an integer value: ");
+    scanf("%d", &input);
+    return input;
+}
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Get_Sum
+ *  Description:  Return the sum of two numbers
+ *  Param1: First integer
+ *  Param2: Second integer
+ *  Return: First + Second
+ * =====================================================================================
+ */
+
+int Get_Sum(int num1,int num2)
+{
+    printf("The sum of these number is: \n");
+    return num1 + num2;
+}
+
