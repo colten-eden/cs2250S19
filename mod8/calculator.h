00ff6949e1f67b623e2ef1dac051c5ae1dad3ae7
Colten Eden <colteneden@mail.weber.edu>
2019-02-28 10:12:44 -0700

    calculator.c

---
+++ mod8/calculator.h
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  calculator.h
+ *
+ *    Description:  Calculator library
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2019 09:56:28 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Colten Eden (), colteneden@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#ifndef  CALCULATOR__INC__
+#define  CALCULATOR__INC__
+// Constants
+
+// Prototypes
+
+int Add_Numbers(int num1, int num2);
+int Add_Numbers(int num1, int num2);
+#endif /* ----- #ifndef CALCULATOR__INC__ ----- */
+
+ /* ----- #ifndef CALCULATOR__INC__ ----- */
+
